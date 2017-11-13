626dbfac89bb140e051f25598810563167287032
oleg.olekseenko <olekseenkoo3@gmail.com>
2017-11-13 17:36:09 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
+/**
+* * @author Oleg Olekseenko (olekseenkoo3@gmail.com)
+* * @since 13.11.2017
+*/
 class Calculate{
 public static void main(String[] args)
 {
 System.out.println("Hello World");
+/**
+*Приветствие
+*
+*/
 }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* * @author Oleg Olekseenko (olekseenkoo3@gmail.com)
+* * @since 13.11.2017
+*/
+/**
+*Приветствие
+*
+*/
