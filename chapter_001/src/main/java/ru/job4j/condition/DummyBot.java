c6c02acfae6848b2cde9c0c73d1cb5e1e97a81d8
oleg.olekseenko <olekseenkoo3@gmail.com>
2017-11-23 09:22:04 +0300

    3.1. Глупый бот.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+/**
+ * @author Oleg Olekseenko (olekseenkoo3@gmail.com)
+ * @version 1.5
+ * @since 21.11.2017
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        return "Привет Бот";
+}
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Oleg Olekseenko (olekseenkoo3@gmail.com)
+ * @version 1.5
+ * @since 21.11.2017
+ */
+
+public class DummyBotTest {
+
+        public void whenGreetBot() {
+            DummyBot bot = new DummyBot();
+            assertThat(
+                    bot.answer("Привет, Бот."),
+                    is("Привет, умник."));
+        }
+
+        public void whenByuBot() {
+            DummyBot bot = new DummyBot();
+            assertThat(
+                    bot.answer("Пока."),
+                    is("До скорой встречи."));
+        }
+        public void whenUnknownBot() {
+            DummyBot bot = new DummyBot();
+            assertThat(
+                    bot.answer("Сколько будет 2 + 2?"),
+                    is("Это ставит меня в тупик. Спросите другой вопрос."));
+        }
+    }
+
+
+
