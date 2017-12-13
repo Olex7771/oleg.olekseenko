15509f97b90e3b687c1d131e2141ecf05d043b19
oleg.olekseenko <olekseenkoo3@gmail.com>
2017-12-13 12:43:06 +0300

    3.1 Глупый бот

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,8 +1,9 @@
 package ru.job4j.condition;
+
 /**
- * @author Oleg Olekseenko (olekseenkoo3@gmail.com)
- * @version 1.5
- * @since 21.11.2017
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
  */
 public class DummyBot {
     /**
@@ -11,6 +12,16 @@ public class DummyBot {
      * @return Ответ.
      */
     public String answer(String question) {
-        return "Привет Бот";
-}
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, что этот вопрос известен боту и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        } else if ("Сколько будет 2 + 2?".equals(question)) {
+            rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        }
+        return rsl;
+    }
 }
